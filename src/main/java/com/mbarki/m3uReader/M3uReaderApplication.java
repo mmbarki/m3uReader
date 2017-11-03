0cc31fafe77c9c4076cc0cb157ab7f1b491ddd5b
mmbarki <mmbarki.ext@orange.com>
2017-11-03 16:15:35 +0100

    init spring project

---
+++ src/main/java/com/mbarki/m3uReader/M3uReaderApplication.java
@@ -0,0 +1,12 @@
+package com.mbarki.m3uReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3uReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3uReaderApplication.class, args);
+	}
+}
